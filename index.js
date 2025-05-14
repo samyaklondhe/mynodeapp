3dc3ddd79a316e7d4f33b9792011df0f0b5de814
Sam <sam@gmail.com>
2025-05-14 10:15:59 +0000

    commit deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-        res.send('Hello and Welcome from Nodejs App');
+        res.send('Hello and Welcome from Nodejs App from Deploy project');
 });
 
 const PORT = process.env.PORT || 3000;
